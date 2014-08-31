744f2cd01541dc2bd504dcafcd299039f98b855f
Shinmera <shinmera@tymoon.eu>
2014-08-31 13:10:49 +0200

    Fixes to BLOG/ api urls and calls

--- blog.lisp
+++ blog.lisp
@@ -8,13 +8,13 @@
 
 (defvar *blog/info* "https://api.tumblr.com/v2/blog/~a.tumblr.com/info")
 (defvar *blog/avatar* "https://api.tumblr.com/v2/blog/~a.tumblr.com/avatar/~a")
-(defvar *blog/likes* "https://api.tumblr.com/v2/blog/~a.tumblr.com/")
-(defvar *blog/followers* "https://api.tumblr.com/v2/blog/~a.tumblr.com/")
+(defvar *blog/likes* "https://api.tumblr.com/v2/blog/~a.tumblr.com/likes")
+(defvar *blog/followers* "https://api.tumblr.com/v2/blog/~a.tumblr.com/followers")
 
 (defun blog/info (username)
   (request (format NIL *blog/info* username) :parameters `(("api_key" . ,south:*oauth-api-key*))))
 
-(defun blog/avatar (username &key (size 64) (fetch T))
+(defun blog/avatar (username &key (size 64) fetch)
   (assert (find size '(16 24 30 40 48 64 96 128 512))
           () "Size must be one of (16 24 30 40 48 64 96 128 512)")
   (multiple-value-bind (image status headers uri) (request (format NIL *blog/avatar* username size) :redirect (if fetch 1 NIL))
@@ -28,11 +28,11 @@
           () "Limit must be between 1 and 20 (inclusive).")
   (assert (<= 0 offset)
           () "Offset must be positive.")
-  (request (format NIL *blog/likes* username) :parameters (prepare* limit offset)))
+  (request (format NIL *blog/likes* username) :parameters (cons `("api_key" . ,south:*oauth-api-key*) (prepare* limit offset))))
 
 (defun blog/followers (username &key (limit 20) (offset 0))
   (assert (<= 1 limit 20)
           () "Limit must be between 1 and 20 (inclusive).")
   (assert (<= 0 offset)
           () "Offset must be positive.")
-  (request (format NIL *blog/followers* username) :parameters (prepare* limit offset)))
+  (request (format NIL *blog/followers* username) :oauth T :parameters (prepare* limit offset)))
