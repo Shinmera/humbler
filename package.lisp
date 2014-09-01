5b79796f6e727089b3cd6ff91c80918abe44a36a
Shinmera <shinmera@tymoon.eu>
2014-09-01 18:38:27 +0200

    Exporting

--- package.lisp
+++ package.lisp
@@ -46,25 +46,183 @@
 
 (defpackage #:humbler-objects
   (:nicknames #:org.tymoonnext.humbler.objects)
-  ;;
-  (:export)
-  )
+  ;; generics.lisp
+  (:export
+   #:blog=
+   #:post=
+   #:augment
+   #:blog
+   #:followers
+   #:follow
+   #:unfollow
+   #:likes
+   #:submissions
+   #:drafts
+   #:queue
+   #:posts
+   #:post
+   #:save
+   #:destroy
+   #:reblog
+   #:like
+   #:unlike
+   #:refresh
+   #:dashboard
+   #:following
+   #:myself
+   #:my-followers
+   #:my-likes
+   #:my-submissions
+   #:my-drafts
+   #:my-queue
+   #:my-posts)
+  ;; object-bridge.lisp
+  (:export
+   #:make-photo
+   #:make-photo-size
+   #:make-dialogue
+   #:make-video-player
+   #:make-blog
+   #:make-user
+   #:make-post)
+  ;; objects.lisp
+  (:export
+   #:blog
+   #:name
+   #:url
+   #:title
+   #:avatar
+   #:description
+   #:updated
+   #:draft-count
+   #:queue-count
+   #:message-count
+   #:post-count
+   #:like-count
+   #:share-likes
+   #:admin
+   #:facebook-opengraph-enabled
+   #:twitter-send
+   #:twitter-enabled
+   #:can-send-fan-mail
+   #:followed
+   #:ask-p
+   #:ask-anon
+   #:ask-page-title
+   #:nsfw-p
+   
+   #:user
+   #:following-post
+   #:default-post-format
+   #:blogs
+   
+   #:post
+   #:id
+   #:post-type
+   #:blog-name
+   #:post-url
+   #:timestamp
+   #:date
+   #:text-format
+   #:reblog-key
+   #:tags
+   #:bookmarklet
+   #:mobile
+   #:source-url
+   #:source-title
+   #:liked
+   #:state
+   #:post-format
+   #:tweet
+   #:slug
+   
+   #:text-post
+   #:title
+   #:body
+   
+   #:photo-post
+   #:photos
+   #:file
+   #:caption
+   #:width
+   #:height
+   
+   #:quote-post
+   #:text
+   #:source
+   
+   #:link-post
+   #:title
+   #:url
+   #:description
+   
+   #:chat-post
+   #:title
+   #:body
+   #:dialogue
+   
+   #:audio-post
+   #:caption
+   #:file
+   #:player
+   #:play-count
+   #:album-art
+   #:artist
+   #:album
+   #:track-name
+   #:track-number
+   #:year
+   
+   #:video-post
+   #:caption
+   #:file
+   #:players
+   
+   #:answer-post
+   #:asking-name
+   #:asking-url
+   #:question
+   #:answer
+   
+   #:photo
+   #:caption
+   #:sizes
+   
+   #:photo-size
+   #:width
+   #:height
+   #:url
+   
+   #:dialogue
+   #:label
+   #:name
+   #:phrase
+   
+   #:video-player
+   #:width
+   #:embed-code))
 
 (defpackage #:humbler-extra
   (:nicknames #:org.tymoonnext.humbler.extra)
   ;; auth.lisp
   (:export
+   #:*user*
    #:login
    #:logout)
   ;; toolkit.lisp
   (:export
+   #:*tumblr-datetime-format*
    #:to-keyword
    #:from-keyword
    #:raw-request
    #:request
    #:prepare
    #:prepare*
-   #:get-unix-time))
+   #:get-unix-time
+   #:format-tumblr-date
+   #:parse-tumblr-date
+   #:pageinate
+   #:print-slots))
 
 (defpackage #:humbler
   (:nicknames #:org.tymoonnext.humbler)
